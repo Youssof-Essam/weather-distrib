cd55c9d8fab6d070b1339a07b251c8c3e7a02f7f
Youssof Elroweini <elroweiniyoussof@gmail.com>
2025-12-04 22:12:48 -0500

    formatting

--- src/mapper.py
+++ src/mapper.py
@@ -14,7 +14,7 @@ for row in csv_reader:
         wind_speed = float(wind_speed)
         wind_dir = float(wind_dir)
         rain = float(rain)
-        # Emit: city <tab> temp,humidity,1
+        # Emit: city <tab> temp,humidity,dew, pressure, wind_speed, wind_dir, rain,1
         print(f"{location_id}\t{temp},{humidity},{dew},{pressure},{wind_speed},{wind_dir},{rain},1")
 
     except:
